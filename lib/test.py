3204ec621a385fca90de5444d92a17f2482f8ed0
Charles Sibbald <casibbald@gmail.com>
2012-03-19 17:26:35 +0000

    xml class methods

--- lib/test.py
+++ lib/test.py
@@ -8,6 +8,24 @@ dom = read_xml(myxml_file)
 
 if __name__ == '__main__':
     print dom
+dom_methods = ['ATTRIBUTE_NODE', 'CDATA_SECTION_NODE', 'COMMENT_NODE', 'DOCUMENT_FRAGMENT_NODE',
+'DOCUMENT_NODE', 'DOCUMENT_TYPE_NODE', 'ELEMENT_NODE', 'ENTITY_NODE', 'ENTITY_REFERENCE_NODE',
+'NOTATION_NODE', 'PROCESSING_INSTRUCTION_NODE', 'TEXT_NODE', '__doc__', '__init__', '__module__', 
+'__nonzero__', '_call_user_data_handler', '_child_node_types', '_create_entity', '_create_notation',
+'_elem_info', '_get_actualEncoding', '_get_async', '_get_childNodes', '_get_doctype',
+'_get_documentElement', '_get_documentURI', '_get_elem_info', '_get_encoding', '_get_errorHandler',
+'_get_firstChild', '_get_lastChild', '_get_localName', '_get_standalone', '_get_strictErrorChecking',
+'_get_version', '_id_cache', '_id_search_stack', '_magic_id_count', '_set_async', 'abort',
+'actualEncoding', 'appendChild', 'async', 'attributes', 'childNodes', 'cloneNode', 'createAttribute',
+'createAttributeNS', 'createCDATASection', 'createComment', 'createDocumentFragment', 'createElement',
+'createElementNS', 'createProcessingInstruction', 'createTextNode', 'doctype', 'documentElement',
+'documentURI', 'encoding', 'errorHandler', 'firstChild', 'getElementById', 'getElementsByTagName',
+'getElementsByTagNameNS', 'getInterface', 'getUserData', 'hasChildNodes', 'implementation',
+'importNode', 'insertBefore', 'isSameNode', 'isSupported', 'lastChild', 'load', 'loadXML',
+'localName', 'namespaceURI', 'nextSibling', 'nodeName', 'nodeType', 'nodeValue', 'normalize',
+'ownerDocument', 'parentNode', 'prefix', 'previousSibling', 'removeChild', 'renameNode',
+'replaceChild', 'saveXML', 'setUserData', 'standalone', 'strictErrorChecking', 'toprettyxml',
+'toxml', 'unlink', 'version', 'writexml']
 #    deployment = dom.getElementsByTagName('deployment')[0]
 
 #    deployment.childNodes[15].nodeName
