016c4a129518ddbf7be00d8a54d809ba846c9f86
Charles Sibbald <casibbald@gmail.com>
2013-01-08 16:58:08 +0000

    added tests directory

--- jbctl.py
+++ jbctl.py
@@ -1,2 +1,26 @@
-from lib import netstat
+#!/usr/bin/env python
+###############################################################################
+#
+# Author          : Charles Sibbald
+# Email           : casibbald at gmail dot com
+# Contributors    : 
+# License         : BSD
+# Copyright (c) 2012 Origami Planes Ltd.
+# All rights reserved, excluding that of external software or modules
+# from 3rd Parties.
+# Redistribution and use in source and binary forms are permitted
+# provided that the above copyright notice and this paragraph are
+# duplicated in all such forms and that any documentation,
+# advertising materials, and other materials related to such
+# distribution and use acknowledge that the software was developed
+# by the <organization>.  The name of the
+# University may not be used to endorse or promote products derived
+# from this software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# 
+###############################################################################
+
+
 
